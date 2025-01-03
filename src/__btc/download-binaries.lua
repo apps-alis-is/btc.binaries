e1787889702aff0c79af345c3664cc1cecf9a48b
ciripel <37701673+ciripel@users.noreply.github.com>
2025-01-04 00:03:50 +0200

    feat: snake case migration (#1)
    * add github workflow
    * applied eli patcher
    * manual migration
    * removed _prefix

--- src/__btc/download-binaries.lua
+++ src/__btc/download-binaries.lua
@@ -1,59 +1,59 @@
 assert(fs.EFS, "eli.fs.extra required")
 
-local _ok, _error = fs.safe_mkdirp("bin")
-ami_assert(_ok, string.join_strings("Failed to prepare bin dir: ", _error), EXIT_APP_IO_ERROR)
+local ok, err = fs.safe_mkdirp("bin")
+ami_assert(ok, string.join_strings("Failed to prepare bin dir: ", err), EXIT_APP_IO_ERROR)
 
-local extractArchiveFns = {
-    zip = function (src, dst, options) 
-        local _ok, _error = zip.safe_extract(src, dst, options)
+local extract_archive_fns = {
+    zip = function(src, dst, options)
+        local ok, err = zip.safe_extract(src, dst, options)
         fs.remove(src)
-        ami_assert(_ok, "Failed to extract: " .. tostring(_error))
+        ami_assert(ok, "Failed to extract: " .. tostring(err))
     end,
-    ["tar"] = function (src, dst, options)
-        local _ok, _error = tar.safe_extract(src, dst, options)
+    ["tar"] = function(src, dst, options)
+        local ok, err = tar.safe_extract(src, dst, options)
         fs.remove(src)
-        ami_assert(_ok, "Failed to extract: " .. tostring(_error))
+        ami_assert(ok, "Failed to extract: " .. tostring(err))
     end,
-    ["tar.gz"] = function (src, dst, options)
-        local _tmpFile = os.tmpname()
-        local _ok, _error = lz.safe_extract(src, _tmpFile)
+    ["tar.gz"] = function(src, dst, options)
+        local tmp_file = os.tmpname()
+        local ok, err = lz.safe_extract(src, tmp_file)
         fs.remove(src)
-        if not _ok then
-            fs.remove(_tmpFile)
-            ami_error("Failed to extract: " .. tostring(_error))
+        if not ok then
+            fs.remove(tmp_file)
+            ami_error("Failed to extract: " .. tostring(err))
         end
 
-        local _ok, _error = tar.safe_extract(_tmpFile, dst, options)
-        fs.remove(_tmpFile)
-        ami_assert(_ok, "Failed to extract: " .. tostring(_error))
+        local ok, err = tar.safe_extract(tmp_file, dst, options)
+        fs.remove(tmp_file)
+        ami_assert(ok, "Failed to extract: " .. tostring(err))
     end
 }
 
 local function download_and_extract(url, dst, options)
-    local _tmpFile = os.tmpname()
-    local _ok, _error = net.safe_download_file(url, _tmpFile, {followRedirects = true})
-    if not _ok then
-        fs.remove(_tmpFile)
-        ami_error("Failed to download: " .. tostring(_error))
+    local tmp_file = os.tmpname()
+    local ok, err = net.safe_download_file(url, tmp_file, { follow_redirects = true })
+    if not ok then
+        fs.remove(tmp_file)
+        ami_error("Failed to download: " .. tostring(err))
     end
 
-    local _archiveKind = am.app.get_model("DAEMON_ARCHIVE_KIND", "zip")
-    local _exctractFn = extractArchiveFns[_archiveKind]
-    ami_assert(type(_exctractFn) == "function", "Unsupported daemon archive kind " .. tostring(_archiveKind) .. "!")
-    _exctractFn(_tmpFile, dst, options)
+    local archive_kind = am.app.get_model("DAEMON_ARCHIVE_KIND", "zip")
+    local _extract_fn = extract_archive_fns[archive_kind]
+    ami_assert(type(_extract_fn) == "function", "Unsupported daemon archive kind " .. tostring(archive_kind) .. "!")
+    _extract_fn(tmp_file, dst, options)
 end
 
 log_info("Downloading " .. am.app.get_model("DAEMON_NAME") .. "...")
-download_and_extract(am.app.get_model("DAEMON_URL"), "bin", {flattenRootDir = true, openFlags = 0})
+download_and_extract(am.app.get_model("DAEMON_URL"), "bin", { flatten_root_dir = true, open_flags = 0 })
 
-local _ok, _files = fs.safe_read_dir("bin", { returnFullPaths = true})
-ami_assert(_ok, "Failed to enumerate binaries", EXIT_APP_IO_ERROR)
+local ok, files = fs.safe_read_dir("bin", { return_full_paths = true }) --[[@as DirEntry]]
+ami_assert(ok, "Failed to enumerate binaries", EXIT_APP_IO_ERROR)
 
-for _, file in ipairs(_files) do 
-    if fs.file_type(file) == 'file' then 
-        local _ok, _error = fs.safe_chmod(file, "rwxrwxrwx")
-        if not _ok then 
-            ami_error("Failed to set file permissions for " .. file .. " - " .. _error, EXIT_APP_IO_ERROR)
+for _, file in ipairs(files) do
+    if fs.file_type(file) == 'file' then
+        local ok, err = fs.safe_chmod(file, "rwxrwxrwx")
+        if not ok then
+            ami_error("Failed to set file permissions for " .. file .. " - " .. err, EXIT_APP_IO_ERROR)
         end
     end
 end
